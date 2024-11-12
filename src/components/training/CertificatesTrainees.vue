d3d1192fdc8a0ba0306dadd2334f371b11302a91
Aintzane <aintzagoffard@gmail.com>
2024-11-12 10:29:56 +0100

    proyecto para corregir

---
+++ src/components/training/CertificatesTrainees.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <div class="comp-card">
+            <div class="comp-text">
+                <h2>{{ $t('message.trainingmaterial3title') }}</h2>
+                <p>{{ $t('message.trainingmaterial3text') }}</p>
+            </div>
+            
+            <div class="comp-downloads">
+                <button class="btn" @click="handleFile($t('message.trainingmaterial3download1'))">{{ $t('message.btntrainingmaterial3download1') }}</button>
+                <button class="btn" @click="handleFile($t('message.trainingmaterial3download2'))">{{ $t('message.btntrainingmaterial3download2') }}</button>
+                <button class="btn" @click="handleFile($t('message.trainingmaterial3download3'))">{{ $t('message.btntrainingmaterial3download3') }}</button>
+                <button class="btn" @click="handleFile($t('message.trainingmaterial3download4'))">{{ $t('message.btntrainingmaterial3download4') }}</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
+
+<script setup>
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+const handleFile = (fileName) => {
+    const fileExtension = fileName.split('.').pop().toLowerCase();
+    const fileUrl = `/archives/Training certificates for trainees/${fileName}`;
+    // Check if the URL is correct and file exists
+    console.log('Attempting to download:', fileUrl);
+    
+    if (fileExtension === 'pdf') {
+        // Open PDFs in a new tab
+        window.open(fileUrl, '_blank');
+    } else {
+        // Download other types of files
+        const link = document.createElement('a');
+        link.href = fileUrl;
+        link.download = fileName;
+        link.click();
+    }
+};
+</script>
---
+++ src/components/training/TTWBL.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="comp-card">
+            <div class="comp-text">
+                <h2>{{ $t('message.trainingmaterial10title') }}</h2>
+                <p>{{ $t('message.trainingmaterial10text') }}</p>
+        
+            </div>
+            
+            <div class="comp-downloads">
+                <button class="btn" @click="handleFile($t('message.trainingmaterial10download1'))">{{ $t('message.btntrainingmaterial10download1') }}</button>
+                <button class="btn" @click="handleFile($t('message.trainingmaterial10download2'))">{{ $t('message.btntrainingmaterial10download2') }}</button>
+
+            </div>
+            
+        </div>
+    
+    </div>
+</template>
+
+<style scoped>
+</style>
+
+<script setup>
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
+const handleFile = (fileName) => {
+const fileExtension = fileName.split('.').pop().toLowerCase();
+const fileUrl = `/archives/training for tutors and trainers in WBL/${fileName}`;
+if (fileExtension === 'pdf') {
+  // Abrir PDFs en una nueva pestaña
+  window.open(fileUrl, '_blank');
+} else {
+  // Descargar otros tipos de archivos
+  const link = document.createElement('a');
+  link.href = fileUrl;
+  link.download = fileName;
+  link.click();
+}
+};
+</script>
